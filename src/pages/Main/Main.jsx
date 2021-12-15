5c50b2bd88bdb3274c397dc4efd21db9bc78e072
eastanov <elmanastanov@gmail.com>
2021-12-15 19:39:49 +0400

    first

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+    return (<div id="main-layout">
+        <div className="sidebar">
+            Sidebar
+        </div>
+        <div className="pages">
+            <h1>Pages</h1>
+        </div>
+    </div>)
+}
+
+export default Main;
