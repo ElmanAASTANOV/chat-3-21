d86dad18f9c911687938f912d6a10db0d15e1ae5
EmilAlizade <emilelizade932@gmail.com>
2021-12-17 19:10:28 +0400

    last version

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Main from 'pages/Main';
 import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineMore } from "react-icons/ai";
+
 
 
 
 function App() {
   return (
     <div className="App">
-      <Main /> <AiOutlineSearch />
+      <Main /> 
     </div>
   );
 }
--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,28 +1,33 @@
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineMore } from "react-icons/ai";
+import { AiOutlineBell } from "react-icons/ai";
 const Notifications = () => {
     return (
         <div id="not" >
             <div className="top">
-             <p> Search</p> 
+                
+            <p> <i className="icon_1"><AiOutlineSearch /> </i> Search  <i className="icon_2"> <AiOutlineMore /> </i></p> 
+            
             
             </div>
 
 
             <div className="bottom"> 
-            <p>Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim <br />
+            <p> <i><AiOutlineBell/></i>Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim <br />
                 tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.</p>
                 <hr />
-            <p>So yes, the alcohol (ethanol) in hand sanitizers can be absorbed through the skin, but no, it would not cause intoxication.</p>
+               <p>  <i className="icon_3"><AiOutlineBell/></i>So yes, the alcohol (ethanol) in hand sanitizers can be absorbed through the skin, but no, it would not cause intoxication.</p>
             <hr />
-            <p>How a visual artist redefines success in graphic design</p>
+            <p> <i className="icon_4" ><AiOutlineBell/></i>How a visual artist redefines success in graphic design</p>
             <hr />
-            <p>For athletes, high altitude produces two contradictory effects on performance. For explosive events (sprints up to 400 metres,  <br /> long jump, triple jump) the reduction in atmospheric pressure means there is less resistance from the atmosphere and the <br /> athlete's performance will generally be better at high altitude. </p>
+            <p> <i><AiOutlineBell/></i>For athletes, high altitude produces two contradictory effects on performance. For explosive events (sprints up to 400 metres,  <br /> long jump, triple jump) the reduction in atmospheric pressure means there is less resistance from the atmosphere and the <br /> athlete's performance will generally be better at high altitude. </p>
             <hr />
-            <p>Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu <br /> cursus vitae congue  </p>
+            <p> <i><AiOutlineBell/></i>Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum a arcu <br /> cursus vitae congue  </p>
             <hr />
-            <p>In fermentum posuere urna nec  </p>
+            <p> <i className="icon_6"><AiOutlineBell/></i>In fermentum posuere urna nec  </p>
             <hr />
-            <p>ID: 22739  </p> <hr />
-            <p> How We Keep Brand Consistency in Our Visual Language — A Design System for Illustrations  </p>
+            <p> <i className="icon_7"><AiOutlineBell/></i>ID: 22739  </p> <hr />
+            <p>  <i className="icon_8"><AiOutlineBell/></i>How We Keep Brand Consistency in Our Visual Language — A Design System for Illustrations  </p>
 
             </div>
 
