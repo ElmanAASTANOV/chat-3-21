58c0d34474bb53338921ff3112119fac958e74a0
LamanMuradzada <lamanmuradzada@gmail.com>
2021-12-24 20:45:54 +0400

    main-layout

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,31 +1,31 @@
 import LogOut from 'menus/LogOut';
 import { useNavigate } from 'react-router-dom';
 
-const SideBar = ({ menus =[], onChangeMenu = () =>{}}) => {
+const SideBar = ({ menus = [], onChangeMenu = () => { } }) => {
     const navigate = useNavigate()
     return (<aside className="side-bar">
         <header>
-          
+
         </header>
 
         <ul>
             {
                 menus?.map((menu) => {
                     return (
-                        <li key = {menu.key} onClick={() =>{
-                            if(onChangeMenu) onChangeMenu(menu.key)
+                        <li key={menu.key} onClick={() => {
+                            if (onChangeMenu) onChangeMenu(menu.key)
                         }}>
-                            
-                           {menu.icon}
+
+                            {menu.icon}
 
                         </li>
 
-)
-})
+                    )
+                })
 
-}
-   <LogOut style = {{color:"#BAD1FF", fontSize: "4rem"}} onClick = {() => navigate("/login")}/>
+            }
 
+            <LogOut className="logout" onClick={() => navigate("/login")} />
         </ul>
     </aside>)
 }
