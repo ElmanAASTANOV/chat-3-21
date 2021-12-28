76144d1e69323f9a55e0ca36cdbfbfd30950a50b
Elvin Mammadli <mamedli.elvin@gmail.com>
2021-12-28 15:17:31 +0400

    route to homepage fixed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,11 +6,11 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ReactLoading from "react-loading"
 import { LS } from "utils";
-import { appConfig} from "config";
+import { appConfig } from "config";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   return (
     <section id="login-page">
       <form
@@ -21,20 +21,20 @@ const Login = () => {
           const password = elements["password"].value;
           setLoading(true);
           LogIn({ username, password })
-          .then(res => {
-            LS.setItemLocalStorage(appConfig.login, JSON.stringify(res.data))
-            navigate("/", { replace: true });
-        })
-        .catch(err => alert("Xeta bas verdi"))
-        .finally(() => { setLoading(false) })
+            .then(res => {
+              LS.setItemLocalStorage(appConfig.login, JSON.stringify(res.data))
+              navigate("/home", { replace: true });
+            })
+            .catch(err => alert("Xeta bas verdi"))
+            .finally(() => { setLoading(false) })
         }}
       >
         <Input name="username" />
         <PasswordInput name="password" />
         <Button>Log In</Button>
         {loading && <div id="form-loading">
-            <ReactLoading type="spin" color="#1877f2"/>
-            </div>}
+          <ReactLoading type="spin" color="#1877f2" />
+        </div>}
       </form>
     </section>
   );
