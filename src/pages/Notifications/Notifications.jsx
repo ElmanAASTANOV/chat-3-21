2f2e1b659fd4c4b718c8fba13380dd7c4d0b0f65
unknown <xeyyamomarov2014@gmail.com>
2022-01-05 18:29:52 +0400

    [xeyyam]/search of notifications was created

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -6,6 +6,7 @@ import { useState,useEffect } from "react";
 
 const Notifications = () => {
     const[notifications,setNotifications]=useState([])
+    const[notificationsSearch,setNotificationsSearch]=useState("")
     useEffect(()=>{  
         getNotifications()
         .then(res=>setNotifications(res))   
@@ -16,7 +17,9 @@ const Notifications = () => {
         <div className="notifications">
         <div className="search-bar">
         <SearchIcon fontSize='' className='search-icon' />
-        <input className='search' type="text" placeholder='Search' />
+        <input className='search' type="text" placeholder='Search' onChange={(event)=>{
+          setNotificationsSearch(event.target.value)
+        }} />
         <div className="three-dots">
           <span className="dot"></span>
           <span className="dot"></span>
@@ -27,7 +30,13 @@ const Notifications = () => {
 
       <div className="content">
                 {
-                    notifications.map((item,index)=>{
+                    notifications.filter(item=>{
+                      if(notificationsSearch===""){
+                        return item
+                      }else if(item.content.toLowerCase().includes(notificationsSearch.toLowerCase())){
+                        return item
+                      }return false
+                    }).map((item,index)=>{
                         return(
                             < div id="content"key={index} >
                         <p><i><AiOutlineBell/></i>{item.content}</p>
